999fa873ecc0d6a812707989b59b68325a42d1a2
Shloimy <shloimyelbaum@gmail.com>
2024-08-21 19:12:13 +0300

    design improvement for video card

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect } from "react";
 import Link from "next/link";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Error({
   error,
@@ -15,38 +17,48 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <main className="h-96 w-full flex flex-col justify-start items-center">
-        <h2 className="text-2xl text-center mx-auto mt-10 w-full">
-          Something went wrong!
-        </h2>
-        <h3 className="text-xl text-center mx-auto my-4 w-full">
+    <>
+      <Header />
+      <div>
+        <main className="h-96 w-full flex flex-col justify-start items-center">
+          <h2 className="text-2xl text-center mx-auto mt-10 w-full">
+            Something went wrong!
+          </h2>
+          <h3 className="text-xl text-center mx-auto my-4 w-full">
             {error.message}
-        </h3>
-        <p className="text-center mx-auto mt-4 w-full">
+          </h3>
+          <p className="text-center mx-auto mt-4 w-full">
             Unfortunately we couldn&apos;t retrieve any data.
-        </p>
-        <p className="text-center mx-auto mt-4 w-full">
-            Please reach out to me at <a href="mailto:shloimyelbaum@gmail.com" className="text-blue-600 hover:underline">shloimyelbaum@gmail.com</a>
-        </p>
-        <div className="flex gap-4 justify-center items-center">
-          <button
-            className="bg-primary-blue hover:bg-blue-700 active:scale-90 text-white font-bold py-2 px-4 rounded"
-            onClick={
-              // Attempt to recover by trying to re-render the segment
-              () => reset()
-            }
-          >
-            Try again
-          </button>
-          <Link
-            href={`/`}
-            className="bg-primary-blue hover:bg-blue-700 active:scale-90 text-white font-bold py-2 px-4 rounded"
-          >
-            Return to Home
-          </Link>
-        </div>
-      </main>
-    </div>
+          </p>
+          <p className="text-center mx-auto mt-4 w-full">
+            Please reach out to me at{" "}
+            <a
+              href="mailto:shloimyelbaum@gmail.com"
+              className="text-blue-600 hover:underline"
+            >
+              shloimyelbaum@gmail.com
+            </a>
+          </p>
+          <div className="flex gap-4 justify-center items-center">
+            <button
+              className="bg-primary-blue hover:bg-blue-700 active:scale-90 text-white font-bold py-2 px-4 rounded"
+              onClick={
+                // Attempt to recover by trying to re-render the segment
+                () => reset()
+              }
+            >
+              Try again
+            </button>
+            <Link
+              href={`/topics`}
+              className="bg-primary-blue hover:bg-blue-700 active:scale-90 text-white font-bold py-2 px-4 rounded"
+            >
+              Go to Topics
+            </Link>
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 }
