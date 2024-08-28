1350288521d7235d7ad4becfdc2b5e0edf78ae1e
Shloimy <shloimyelbaum@gmail.com>
2024-08-28 16:25:18 +0300

    changed videoId route to use getSession and added auth_token to session

--- src/app/api/videos/[videoId]/route.ts
+++ src/app/api/videos/[videoId]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import fetch from "node-fetch";
 import https from "https";
-import { cookies } from "next/headers";
+import { getSession } from "next-auth/react";
 
 const agent = new https.Agent({
   rejectUnauthorized: false,
@@ -19,13 +19,15 @@ type Props = {
  * @description This function handles the GET request to retrieve one video by vdeo_id.
  */
 export async function GET(request: NextRequest, { params }: Props): Promise<Response> {
+  const session = await getSession({})
   // Get /video_id from the request
   console.log("videoById: Request received for video_id:", params.videoId);
-  console.log("videoById: request ", request);
-  console.log("videoById: cookies", request.cookies);
   const { videoId } = params;
-  const authToken = request.cookies.get("auth_token")?.value
+  const authToken = session?.accessToken || null;
   console.log("authToken", authToken);
+  if (!authToken){
+    console.log("videoById: authToken was not found in cookies")
+  }
 
   if (!videoId) {
     // Return a 400 Bad Request if no video_id is provided
@@ -36,7 +38,7 @@ export async function GET(request: NextRequest, { params }: Props): Promise<Resp
   }
 
   const response = await fetch(
-    `https://mttbackend-production.up.railway.app/api/videos/${videoId}/`,
+    `${process.env.BACKEND_URL}/api/videos/${videoId}/`,
     {
       method: "GET",
       headers: {
