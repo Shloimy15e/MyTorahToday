bd1d43abd181b067e6889a48c77f1ea74e8e3a95
Shloimy <shloimyelbaum@gmail.com>
2024-09-13 14:18:09 +0300

    typo

--- src/app/(inner layout)/videos/watched/page.tsx
+++ src/app/(inner layout)/videos/watched/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
 
-export default async function WatchdVideos() {
+export default async function WatchedVideos() {
   const authToken = cookies().get("auth_token")?.value || null;
   try {
     if (!authToken) {
       console.log("You must be logged in to have watched videos");
-      throw new Error("You must be logged in to have watchd videos");
+      throw new Error("You must be logged in to have watched videos");
     }
     const url = `${process.env.BACKEND_URL}/api/videos/?is_viewed_by_user=true`;
     console.log(url);
@@ -30,7 +30,7 @@ export default async function WatchdVideos() {
           {videos && videos.length > 0 && (
             <VideoGrid
               videos={videos}
-              title={"Videos you watchd"}
+              title={"Videos you watched"}
               topicName={"watched videos"}
               showAll={true}
               topicVideos={false}
@@ -39,7 +39,7 @@ export default async function WatchdVideos() {
           )}
           {videos && videos.length === 0 && (
             <div className="flex flex-col items-center justify-center h-96">
-              <h1 className="text-xl font-bold">No watchd videos found</h1>
+              <h1 className="text-xl font-bold">No watched videos found</h1>
               <Link
                 href="/topics"
                 className="md:text-lg bg-primary-blue text-gray-100 text-center font-semibold md:px-6 py-2 rounded-md shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300 cursor-pointer hover:bg-blue-950 mx-14 md:mx-24 my-6 w-4/5"
